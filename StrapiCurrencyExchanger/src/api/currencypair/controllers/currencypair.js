9209a6191677785ad42e53cc04edf5d25651671b
AliAlahmad1981 <ali.alabdalahmad@utb.ecutbildning.se>
2023-04-15 23:29:21 +0200

    adding strapi currency exchanger backend

---
+++ StrapiCurrencyExchanger/src/api/currencypair/controllers/currencypair.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * currencypair controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::currencypair.currencypair');
---
+++ StrapiCurrencyExchanger/src/api/currencypair/routes/currencypair.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * currencypair router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::currencypair.currencypair');
